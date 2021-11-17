1f2d489ac8c2f65210a18d32f53acf0f80064186
ayano-w <ayasaku.t5@gmail.com>
2021-11-17 06:01:21 +0000

    Add models

---
+++ db/migrate/20211117052127_add_colums_to_customers.rb
@@ -0,0 +1,15 @@
+class AddColumsToCustomers < ActiveRecord::Migration[5.2]
+  def change
+
+    add_column :customers, :last_name, :string
+    add_column :customers, :first_name, :string
+    add_column :customers, :kana_last_name, :string
+    add_column :customers, :kana_first_name, :string
+    add_column :customers, :postal_code, :string
+    add_column :customers, :address, :string
+    add_column :customers, :telephone_number, :string
+    add_column :customers, :is_deleted, :boolean
+
+
+  end
+end
