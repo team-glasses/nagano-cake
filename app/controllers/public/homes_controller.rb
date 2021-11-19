9e2e12650a160f282c0d591aab9333b9be85ac8a
scarletgate <xiaoyuedu10@gmail.com>
2021-11-19 12:33:40 +0000

    add-public/items

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
+    @categories = Category.all
   end
 
   def about
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,11 @@
 class Public::ItemsController < ApplicationController
   def index
+    @categories = Category.all
+    @items = Item.page(params[:page]).reverse_order
   end
 
   def show
+    @categories = Category.all
+    @item = Item.find(params[:id])
   end
 end
