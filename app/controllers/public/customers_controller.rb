6d10ff53f0264f4635011ac06654110cea9a8315
ueyama1995 <shinji160829033205@gmail.com>
2021-11-19 06:07:54 +0000

    Add customers public

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,10 +1,25 @@
 class Public::CustomersController < ApplicationController
   def show
+
   end
 
   def check
+
   end
 
   def edit
+
   end
+
+  def update
+    @customer.update(customer_params)
+    redirect_to customers_my_page_path
+  end
+
+  def withdraw
+     @customer = customers.find(params[:id])
+     redirect_to root_path
+  end
+
+
 end
