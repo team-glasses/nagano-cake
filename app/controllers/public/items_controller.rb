d5c768e0a49f391d8a00edc5a3f504e0c1f194d0
scarletgate <xiaoyuedu10@gmail.com>
2021-11-21 03:18:30 +0000

    to merge develop

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,12 +1,14 @@
 class Public::ItemsController < ApplicationController
   def index
     @categories = Category.all
-    @items = Item.page(params[:page])
+    @item = Item.where(is_active: 'true')
+    @items = Item.page(params[:page]).where(is_active: 'true')
   end
 
   def show
     @categories = Category.all
     @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
 
   private
