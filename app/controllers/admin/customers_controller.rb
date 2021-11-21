503d9b5c12bb44840269cac780a1df8573427180
scarletgate <xiaoyuedu10@gmail.com>
2021-11-21 08:20:45 +0000

    add_admin/customers-view

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -4,9 +4,22 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
-    @customer = customer.find(params[:id])
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer)
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name,:kana_last_name, :kana_first_name, :postal_code, :address, :telephone_number, :email, :is_deleted)
+  end
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ApplicationRecord
+  attribute :is_deleted, :boolean, default: 'false'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,10 +9,5 @@ class Customer < ApplicationRecord
   has_many :addresses, dependent: :destroy
   has_many :cart_items, dependent: :destroy
   has_many :orders
-
-  #フルネーム表示用のメソッド
-  def full_name
-    self.last_name + self.first_name
-  end
 
 end
