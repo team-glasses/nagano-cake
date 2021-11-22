e4e262d1275daa5037e6f9689a4323b1e66656dd
scarletgate <xiaoyuedu10@gmail.com>
2021-11-22 09:07:55 +0000

    update_admin/orders-show

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,4 +2,17 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.all
   end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    #仮でadminのトップ画面へ遷移させています。
+    redirect_to admin_root_path
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:status)
+  end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,9 @@
 class Order < ApplicationRecord
-  
+  attribute :status, :integer, default: '0'
+
   belongs_to :customer
   has_many :order_items, dependent: :destroy
-  
+
+  enum status: { waiting_for_deposit: 0, payment_confirmation: 1, in_production: 2, preparing_for_shipping: 3, sent: 4 }, _prefix:true
+
 end
