38f40d456168356bc87d9160c2725b1e7479bf3d
AkaneU <akane0419.u0615@gmail.com>
2021-11-22 09:07:15 +0000

    add controller and view and model for order/public

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,23 +1,58 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
   end
 
   def confirm
     @order = Order.new(order_params)
+    if params[:order][:selected_address] == "0"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:selected_address] == "1"
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    elsif params[:order][:selected_address] == "2"
+      @order = Order.new(address_params)
+    end
   end
 
   def complete
   end
 
+  def create
+    cart_items = current_customer.cart_items.all
+    @order = current_customer.orders.new(order_params)
+    if @order.save
+      cart_items.each do |cart_item|
+        order_item = OrderItem.new
+        order_item.item_id = cart_item.id
+        order_item.order_id = @order.id
+        order_item.quantity = cart_item.quantity
+        order_item.price = cart_item.item_id
+        order_item.save
+      end
+      redirect_to orders_complete_path
+      cart_items.destroy_all
+    end
+  end
+
   def index
   end
+
 
   def show
   end
 
   private
   def order_params
-    params.require(:order).permit(:payment_method)
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name, :postage)
+  end
+
+  def address_params
+    params.require(:order).permit(:postal_code, :address, :name)
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,13 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items, dependent: :destroy
 
+  
   enum payment_method: { credit_card: 0, transfer: 1 }
+  
+  enum selected_address: { current_customer_address: 0, addresses_of_current_customer: 1, new_address: 2 }
+  
+  def postage_for_order
+    postage = 800
+  end  
 
 end
