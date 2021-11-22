9b1fb26fb6b68fdb499ded5cec5e20b4f49b9de1
ueyama1995 <shinji160829033205@gmail.com>
2021-11-22 05:00:51 +0000

    add_customers_public

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -17,8 +17,8 @@ class Public::CustomersController < ApplicationController
   end
 
   def withdrawal
-    @customer = Customer.find(params[:id])
-    @costomer.update(is_deleted: true)
+    @customer = current_customer
+    @customer.update(is_deleted: true)
     reset_session
     flash[:notice] = "退会処理を実行いたしました"
     redirect_to root_path
