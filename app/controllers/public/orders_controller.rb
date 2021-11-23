cde53c846f190fbfc166b3b95016af549b8fc9fe
ueyama1995 <shinji160829033205@gmail.com>
2021-11-23 07:58:44 +0000

    add_customers_public

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -9,8 +9,11 @@ class Public::OrdersController < ApplicationController
   end
 
   def index
+   @orders = current_customer.orders
   end
 
   def show
+    #@order = Order.find(params[:id])
+   # @order_items = @order.order_items
   end
 end
