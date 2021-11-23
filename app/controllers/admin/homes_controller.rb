8e73c10add731d46c1ef11dd7f9e8ba43a87cfd0
ayano-w <ayasaku.t5@gmail.com>
2021-11-23 07:23:39 +0000

    to merge develop

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,10 +1,16 @@
 class Admin::HomesController < ApplicationController
   def top
-    @orders = Order.all
+    path = Rails.application.routes.recognize_path(request.referer)
+    if path[:controller] == "amdin/customers" && path[:action] == "show"
+      @orders = Order.where(cutomer_id: path[:id])
+    else
+      @orders = Order.all
+    end
   end
-  
-  
-  def total_quantity
-    quantity = find_by(:quontity )
+
+
+  def total_quantity(order_id)
+    total_quantity = OrderItem.grop(:order_id).sum(:quontity)
+    total_quantity.order_id
   end
 end
