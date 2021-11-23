6b442b443ffdfaed04c56e938a204b160c560828
AkaneU <akane0419.u0615@gmail.com>
2021-11-23 07:06:55 +0000

    add controller and view and model for order/public

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -7,33 +7,33 @@ class Public::OrdersController < ApplicationController
     @cart_items = CartItem.all
     @total_price = @cart_items.inject(0){ |sum, item| sum + item.subtotal }
     @order = Order.new(order_params)
-    if params[:order][:selected_address] == "0"
+    if params[:order][:select_address] == "0"
+
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
       @order.name = current_customer.first_name + current_customer.last_name
-    elsif params[:order][:selected_address] == "1"
-      Address.find(params[:order][:address_id])
+    elsif params[:order][:select_address] == "1"
+      @address = Address.find(params[:order][:address_id])
       @order.postal_code = @address.postal_code
       @order.address = @address.address
       @order.name = @address.name
-    elsif params[:order][:selected_address] == "2"
-      
     end
   end
 
   def create
     cart_items = current_customer.cart_items.all
     @order = current_customer.orders.new(order_params)
+
     if @order.save
       cart_items.each do |cart_item|
         order_item = OrderItem.new
         order_item.item_id = cart_item.item_id
         order_item.order_id = @order.id
         order_item.quantity = cart_item.quantity
-        order_item.price = cart_item.item_id.price
+        order_item.price = cart_item.item.price
         order_item.save
       end
-      redirect_to order_complete_path
+      redirect_to orders_complete_path
       cart_items.destroy_all
     end
   end
@@ -49,7 +49,7 @@ class Public::OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name, :total_payment, :postage)
   end
 
 
