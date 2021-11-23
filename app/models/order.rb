624ae91f0fcfb15fb00eb955e2d23fe46c910414
scarletgate <xiaoyuedu10@gmail.com>
2021-11-23 07:24:51 +0000

    update_public/homes-header

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,13 @@
 class Order < ApplicationRecord
-  attribute :status, :integer, default: '0'
+  attribute :status, :integer, default: 0
 
   belongs_to :customer
   has_many :order_items, dependent: :destroy
 
   enum status: { waiting_for_deposit: 0, payment_confirmation: 1, in_production: 2, preparing_for_shipping: 3, sent: 4 }, _prefix:true
+
+  def total_billing_amount
+    total_payment + postage
+  end
 
 end
