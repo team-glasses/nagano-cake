fcf468abe109b38a3892d2493b2cea5e0f745cb7
ayano-w <ayasaku.t5@gmail.com>
2021-11-24 02:50:20 +0000

    add admin homes controller and view pages

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -3,17 +3,14 @@ class Admin::HomesController < ApplicationController
     path = Rails.application.routes.recognize_path(request.referer)
     @total_quantity = OrderItem.group(:order_id).sum(:quantity)
     if path[:controller] == "amdin/customers" && path[:action] == "show"
-      @orders = Order.where(cutomer_id: path[:id])
+      @orders = Order.where(customer_id: path[:id])
     else
       @orders = Order.all
     end
+      @orders = Order.page(params[:page]).per(10)
   end
 
 
-  # def total_quantity(order_id)
-  #   @total_quantity = OrderItem.group(:order_id).sum(:quantity)
-  #   total_quantity.order_id
-  # end
 end
 
 
