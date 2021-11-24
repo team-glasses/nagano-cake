0d9f0497b1e97ed2736fbdf51c0cb9af75903de7
AkaneU <akane0419.u0615@gmail.com>
2021-11-24 06:38:30 +0000

    add controller and view and model for order/public

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -4,6 +4,6 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :item
 
-  enum making_status: { not_startable: 0, waiting_for_production: 1, in_production: 2, production_completed: 3 }, _prefix:true
+  enum making_status: { not_startable: 0, waiting_for_production: 1, production: 2, production_completed: 3 }, _prefix:true
 
 end
